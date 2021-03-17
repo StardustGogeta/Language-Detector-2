46cf8ee35f66cb9ba8a06abe46bdd9150b82bdfe
StardustGogeta <liam.gerst@gmail.com>
2021-03-17 00:54:28 -0400

    Adding all files up until this point
    Includes main Python and some training data for each language.

---
+++ neuralNet.py
@@ -0,0 +1,73 @@
+# Adapted from https://www.tensorflow.org/tutorials/text/text_classification_rnn
+# Also uses https://www.tensorflow.org/guide/keras/rnn
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import trainingData
+import numpy as np
+import copy, random
+
+def plot_graphs(history, metric):
+  plt.plot(history.history[metric])
+  plt.plot(history.history['val_'+metric], '')
+  plt.xlabel("Epochs")
+  plt.ylabel(metric)
+  plt.legend([metric, 'val_'+metric])
+
+VOCAB_SIZE = 1000
+#MAX_SEQUENCE_LENGTH = 1000
+NUM_LANGUAGES = len(trainingData.language_map)
+
+# Import samples of (text, language), where language is an integer
+# corresponding to a certain written language.
+train_dataset, test_dataset = trainingData.loadData()
+
+# Create text vectorization layer
+encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    #output_sequence_length=MAX_SEQUENCE_LENGTH
+)
+# Set the vocabulary of the encoder
+encoder.adapt(train_dataset.map(lambda text, label: text))
+
+##vocab = np.array(encoder.get_vocabulary())
+##print(vocab[:40])
+
+model = tf.keras.Sequential([
+    encoder,
+    tf.keras.layers.Embedding(
+        input_dim=len(encoder.get_vocabulary()),
+        output_dim=64,
+        # Use masking to handle the variable sequence lengths
+        mask_zero=True),
+##    tf.keras.layers.Bidirectional(tf.keras.layers.SimpleRNN(64)),
+##    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(NUM_LANGUAGES)
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam(1e-4)
+metrics = ['accuracy']
+model.compile(loss=loss_fn, optimizer=optimizer, metrics=metrics)
+
+# 30 to 50 epochs seems to be enough to fit without causing extreme overfitting
+history = model.fit(train_dataset, epochs=40, validation_data=test_dataset, verbose=2)
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
+
+plt.figure(figsize=(16,8))
+plt.subplot(1,2,1)
+plt.axhline(y=1/NUM_LANGUAGES, color='r', linestyle='-')
+plot_graphs(history, 'accuracy')
+plt.ylim(None,1)
+plt.subplot(1,2,2)
+plot_graphs(history, 'loss')
+plt.ylim(0,None)
+plt.show()
---
+++ trainingData.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+train_dir = "./data/train"
+test_dir = "./data/test"
+# Keep track of which index maps to which written language
+# Dragon language doesn't have much to work with, so it is omitted until a larger dataset can be gathered
+language_map = ['english', 'spanish', 'latin']
+
+def loadData():
+    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(train_dir, class_names=language_map)
+    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(test_dir, class_names=language_map)
+    
+##    for text_batch, label_batch in raw_train_ds.take(1):
+##      for i in range(10):
+##        print("Question: ", text_batch.numpy()[i][:20])
+##        print("Label:", label_batch.numpy()[i])
+
+    return raw_train_ds, raw_test_ds
