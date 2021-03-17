3a8016fbb924a1150512cbcd3e2f9a807281e1e3
StardustGogeta <stardustgogeta0@gmail.com>
2021-03-17 18:27:55 -0400

    Randomizing input data selection
    This should make it easier to put in data and auto-balance it between training and validation. It also yields more variation in how the model trains and prevents any unintentional bias in which files I put in which section.

--- trainingData.py
+++ trainingData.py
@@ -1,14 +1,27 @@
 import tensorflow as tf
+import random
 
-train_dir = "./data/train"
-test_dir = "./data/test"
+data_dir = "./data"
 # Keep track of which index maps to which written language
 # Dragon language doesn't have much to work with, so it is omitted until a larger dataset can be gathered
 language_map = ['english', 'spanish', 'latin']
 
+seed = random.randint(0, 255)
+print(f"Using seed {seed} for input data randomization...")
+
 def loadData():
-    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(train_dir, class_names=language_map)
-    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(test_dir, class_names=language_map)
+    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        data_dir,
+        class_names=language_map,
+        seed=seed,
+        validation_split=0.2,
+        subset='training')
+    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        data_dir,
+        class_names=language_map,
+        seed=seed,
+        validation_split=0.2,
+        subset='validation')
     
 ##    for text_batch, label_batch in raw_train_ds.take(1):
 ##      for i in range(10):
